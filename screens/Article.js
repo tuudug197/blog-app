6526bae708ed7640ad5f5e94666805ff66694a4d
Гантөвшин Ганхүү <HP233140@M303-CC0181.local>
2023-07-03 19:03:37 +0800

    first commit

---
+++ screens/Article.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from "react";
+import { View, Text, ScrollView } from "react-native";
+import { useWindowDimensions } from 'react-native';
+import RenderHtml from 'react-native-render-html';
+
+export default function Article({route}) {
+    const { target } = route.params
+    const { width } = useWindowDimensions();
+    const [article, setArticle] = useState()
+    useEffect(() => {
+        fetch(`https://dev.to/api/articles/abbeyperini/${target.slug}`)
+        .then((res) => res.json())
+        .then((data) => setArticle(data))
+    }, [])
+
+    if(!article) return <Text>Loading...</Text>
+
+    return ( 
+        <ScrollView>
+            <RenderHtml
+                contentWidth={width - 20}
+                source={{ html: article.body_html}}
+            />
+        </ScrollView>
+       
+    )
+}
